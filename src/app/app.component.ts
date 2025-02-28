99ecac73cf80b03bd103d11596ea862a8e5229bb
Tolgahan Tom <tomtolgahan@gmail.com>
2025-02-28 10:32:58 +0300

    animasyon eklendi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  standalone: false,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'eCommerceWithAngularSupabase';
+
+  ngOnInit(): void {
+    AOS.init({ once: true });
+  }
 }
--- src/app/component/categories/categories.component.ts
+++ src/app/component/categories/categories.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
   standalone: false,
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.scss'
+  styleUrl: './categories.component.scss',
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements AfterViewInit {
+  constructor(private el: ElementRef) {}
 
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('animate__animated', 'animate__zoomIn');
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    const mainDiv = this.el.nativeElement.querySelector('.main');
+    if (mainDiv) {
+      observer.observe(mainDiv);
+    }
+  }
 }
