6885e3171a394770ff42251b0dab0b4ab15a03eb
Tolgahan Tom <tomtolgahan@gmail.com>
2025-03-03 14:45:22 +0300

    product detay sayfasının ürün resim kısmı yapıldı

--- src/app/component/detail/detail.component.ts
+++ src/app/component/detail/detail.component.ts
@@ -4,8 +4,44 @@ import { Component } from '@angular/core';
   selector: 'app-detail',
   standalone: false,
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.scss'
+  styleUrl: './detail.component.scss',
 })
 export class DetailComponent {
+  selectedIndex = 0;
 
+  productImages = [
+    {
+      image:
+        'https://cdn.dsmcdn.com/ty1549/product/media/images/ty1549/prod/QC/20240916/20/8ebd2c22-cd7b-33ff-be16-c57bf1498654/1_org_zoom.jpg',
+    },
+    {
+      image:
+        'https://cdn.dsmcdn.com/ty1550/product/media/images/ty1549/prod/QC/20240916/20/44a5cfa3-676c-3d12-9a6b-4361202b8206/1_org_zoom.jpg',
+    },
+    {
+      image:
+        'https://cdn.dsmcdn.com/ty1556/product/media/images/ty1556/prod/QC/20240918/09/fe79a250-c0e4-3572-8ac0-28fa2c926a61/1_org_zoom.jpg',
+    },
+    {
+      image:
+        'https://cdn.dsmcdn.com/ty1618/prod/QC/20241224/17/c8be8154-3b16-3fed-9d8d-be2a856a4dc0/1_org_zoom.jpg',
+    },
+  ];
+
+  changeMainImage(index: number) {
+    this.selectedIndex = index;
+  }
+
+  nextImage() {
+    this.productImages.length - 1 > this.selectedIndex
+      ? this.selectedIndex++
+      : (this.selectedIndex = 0);
+  }
+
+  prevImage() {
+    this.selectedIndex =
+      this.selectedIndex === 0
+        ? this.productImages.length - 1
+        : this.selectedIndex - 1;
+  }
 }
