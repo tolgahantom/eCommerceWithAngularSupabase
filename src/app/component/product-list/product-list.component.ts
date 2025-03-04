eddeb3df681335e5e4441c5fe1c4630fde511782
Tolgahan Tom <tomtolgahan@gmail.com>
2025-03-04 09:59:28 +0300

    detay sayfasına rating eklendi

--- src/app/component/product-list/product-list.component.ts
+++ src/app/component/product-list/product-list.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { ProductModel } from '../../models/product-model';
 
 @Component({
   selector: 'app-product-list',
   standalone: false,
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  productList: ProductModel[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productList = this.productService.getAllProduct();
+  }
+
+  getStars(rating: number): string[] {
+    const stars = [];
+    for (let i = 1; i < 6; i++) {
+      if (i < rating) {
+        stars.push('fa-solid fa-star');
+      } else if (i === Math.ceil(rating) && rating % 1 !== 0) {
+        stars.push('fa-solid fa-star-half-stroke');
+      } else {
+        stars.push('fa-regular fa-star');
+      }
+    }
+    return stars;
+  }
+
+  getDiscountPrice(price: number, discount: number) {
+    return Math.round(price * (1 - discount / 100));
+  }
 }
--- src/app/models/product-model.ts
+++ src/app/models/product-model.ts
@@ -1,4 +1,4 @@
-export interface Product {
+export interface ProductModel {
   id: string;
   name: string;
   description: string;
